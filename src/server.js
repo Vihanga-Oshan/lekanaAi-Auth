6ddc864d893af83b0dec539cd60d7c4665271dfa
Vihanga Oshan <128878721+Vihanga-Oshan@users.noreply.github.com>
2025-12-04 20:37:09 +0530

    initial project code

--- src/server.js
+++ src/server.js
@@ -120,6 +120,6 @@ app.get("/", (req, res) => res.send("Auth service running..."));
 // ---------------------------------------------
 // START SERVER
 // ---------------------------------------------
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
